29416b2173741d5e640e5c36dcfbb671cf670003
rsolanov <solanov.roman@yandex.ru>
2025-02-10 21:49:10 +0300

    Добавлен файл Dockerfile.clickhouse

---
+++ init_clickhouse_db.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE src_northwind ENGINE = Atomic;
+
+CREATE TABLE src_northwind.orders
+(
+    order_id Int32,
+    customer_id String,
+    employee_id Int32,
+    order_date Date,
+    required_date Date,
+    shipped_date Date,
+    ship_via Int32,
+    freight Float32,
+    ship_name String,
+    ship_address String,
+    ship_city String,
+    ship_region String,
+    ship_postal_code String,
+    ship_country String
+)
+ENGINE = PostgreSQL('host.docker.internal:5432', 'NorthWind', 'orders', 'default', 'default');
+
+CREATE TABLE src_northwind.order_details
+(
+    order_id Int32,
+    product_id Int32,
+    unit_price Float32,
+    quantity Int32,
+    discount Float32
+)
+ENGINE = PostgreSQL('host.docker.internal:5432', 'NorthWind', 'order_details', 'default', 'default');
+
+CREATE TABLE src_northwind.customers
+(
+	customer_id String,
+	company_name String,
+	contact_name String
+)
+ENGINE = PostgreSQL('host.docker.internal:5432', 'NorthWind', 'customers', 'default', 'default');
